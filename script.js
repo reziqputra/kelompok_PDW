12c69895b77c6c1a638de5bb3d35ca205e96d8e5
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-07-08 08:23:06 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var currentDate = new Date();
+
+    var dateTimeString = currentDate.toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'short'
+    });
+    $('#datetime').text(dateTimeString);
+});
